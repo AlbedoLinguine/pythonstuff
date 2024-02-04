e585cfeb04157a5dfa72558d01baaf333f05740b
AlbedoLinguine <yogevcr@gmail.com>
2024-02-04 11:19:51 -0600

    lil bit macro

--- macro.py
+++ macro.py
@@ -2,8 +2,11 @@ import pyautogui
 import time
 
 print(pyautogui.size())
-pyautogui.moveTo(100,100, duration=0.2)
+# pyautogui.moveTo(100,100, duration=0.2)
 
 # pyautogui.moveRel(100, 100)
+# pyautogui.drag
+# pyautogui.dragRel
+# pyautogui.scroll(-200)
 
-pyautogui.click()
+# pyautogui.click()
